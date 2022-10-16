2f20bb4ff42718e6db9e5ae944db3427e16f5a79
estx07 <87713240+estx07@users.noreply.github.com>
2022-10-15 22:51:45 -0300

    Se agregan plantillas para testear las funciones o metodos

--- Disqueria.py
+++ Disqueria.py
@@ -8,14 +8,32 @@ def main():
     busqueda = consulta.ListarAlbums()
     busqueda.PorGenero("Instrumental")
 
+    #Segunda Lista
     busqueda.PorArtista("Michael","Jackson")
 
+    #Consulta todos los Interpretes
     Listar1 = consulta.ListarInterpretes()
-    Listar1.ListaCompleta
+    Listar1.ListaCompleta()
 
-    carga = cargadatos.Cargar()
-    carga.CargarInterprete("Phil","Collins","UK","https://www.discos.com/lafotodelartista.jpg")
-    
+    # #Consulta todos los Generos
+    # Listar2 = consulta.ListarGeneros()
+    # Listar2.ListaCompleta()
+
+    # #Consulta todos los Formatos
+    # Listar3 = consulta.ListarFormatos()
+    # Listar3.ListaCompleta()
+
+    # #Consulta todas las Discograficas
+    # Listar4 = consulta.ListarDiscograficas()
+    # Listar4.ListaCompleta()
+
+    # #Consulta todos los Temas
+    # Listar5 = consulta.ListarCanciones()
+    # Listar5.ListaCompleta()
+
+    #Test de carga Interprete
+    # carga = cargadatos.Cargar()
+    # carga.CargarInterprete("Phil","Collins","UK","https://www.discos.com/lafotodelartista.jpg")
 
 
 
